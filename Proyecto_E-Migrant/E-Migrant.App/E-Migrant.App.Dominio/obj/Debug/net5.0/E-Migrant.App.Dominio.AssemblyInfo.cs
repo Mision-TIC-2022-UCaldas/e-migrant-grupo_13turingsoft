89beffe4a853eb7283381ad04534e5f98a7ffc51
Walther08 <gonzalezwalther08@gmail.com>
2021-11-27 00:18:45 -0500

    Solución conflictos del Frontend

--- Proyecto_E-Migrant/E-Migrant.App/E-Migrant.App.Dominio/obj/Debug/net5.0/E-Migrant.App.Dominio.AssemblyInfo.cs
+++ Proyecto_E-Migrant/E-Migrant.App/E-Migrant.App.Dominio/obj/Debug/net5.0/E-Migrant.App.Dominio.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("imc.App.Consola")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("E-Migrant.App.Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("imc.App.Consola")]
-[assembly: System.Reflection.AssemblyTitleAttribute("imc.App.Consola")]
+[assembly: System.Reflection.AssemblyProductAttribute("E-Migrant.App.Dominio")]
+[assembly: System.Reflection.AssemblyTitleAttribute("E-Migrant.App.Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
