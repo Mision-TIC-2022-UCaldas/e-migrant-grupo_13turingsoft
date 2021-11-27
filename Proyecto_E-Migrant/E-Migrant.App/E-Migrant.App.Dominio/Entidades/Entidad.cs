8ee9ea250da40bd2a1a4c07ae184cfe86c867231
Andrey Bonilla <bonillandrey2015@gmail.com>
2021-11-27 00:38:14 -0500

    version2 reto33

--- Proyecto_E-Migrant/E-Migrant.App/E-Migrant.App.Dominio/Entidades/Entidad.cs
+++ Proyecto_E-Migrant/E-Migrant.App/E-Migrant.App.Dominio/Entidades/Entidad.cs
@@ -9,6 +9,7 @@ namespace E_Migrant.App.Dominio.Entidades
         public string Nit { get; set; }
         public string Direccion { get; set; }
         public string Ciudad { get; set; }
+        //public string Tefefono { get; set; }
         public string Email { get; set; }
         public string PaginaWeb { get; set; }
         public Sector Sector { get; set; }
