ae84f3d9936bf9c8f75bd31c27cbf3ee6b76500f
MiguelPasaje <cartel-dy@hotmail.com>
2021-11-27 23:25:49 -0500

    correcccion en creacion de pages

--- Proyecto_E-Migrant/E-Migrant.App/E-Migrant.App.Frontend/Pages/CrudMigrante/Migrante.cshtml.cs
+++ Proyecto_E-Migrant/E-Migrant.App/E-Migrant.App.Frontend/Pages/CrudMigrante/Migrante.cshtml.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace E_Migrant.App.Frontend.page.CrudMigrante
+namespace E_Migrant.App.Frontend.page
 {
     public class MigranteModel : PageModel
     {
